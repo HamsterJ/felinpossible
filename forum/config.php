ff7647eba535445aabe50f09751be356b5d4ec1b
benjamin.boutier <benjamin.boutier@f1833780-2aa9-11df-b039-17fb639e06ba>
2012-01-03 00:53:00 +0000

    Migration 3.0.8 => 3.0.10 (sans maj thème)

--- forum/config.php
+++ forum/config.php
@@ -6,7 +6,7 @@ $dbhost = 'localhost';
 $dbport = '';
 $dbname = 'felinpossible';
 $dbuser = 'felinpossible';
-$dbpasswd = 'TAG_PASSWORD_DB';
+$dbpasswd = 'GY3vQUrXnfRte9eB';
 
 $table_prefix = 'phpbb_';
 $acm_type = 'file';
