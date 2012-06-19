85ad5ab3fcd15adf9bde9af7a93021a5f0fe3e16
benjamin.boutier <benjamin.boutier@f1833780-2aa9-11df-b039-17fb639e06ba>
2012-06-19 21:32:22 +0000

    Remove utilities (phpMyAdmin, roundCube, spongestats, mysqlDumper)

--- spongestats/sps.configuration.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('sps_server','localhost');
-define('sps_user','felinpossible');
-define('sps_pass','TAG_PASSWORD_DB');
-define('sps_base','felinpossible');
-define('db_prefix','sps_');
-?>
