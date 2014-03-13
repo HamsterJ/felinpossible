df2c498900aef5b808628a239113684f38c325db
flickflack2104 <abcdez2104@yahoo.fr>
2014-03-13 17:10:52 +0100

    Ajout possibilité de don de 5 ou 10 euros de parrainage

--- site/application/utils/Constantes.php
+++ site/application/utils/Constantes.php
@@ -52,9 +52,11 @@ class FP_Util_Constantes {
 	 * Dons possibles pour le parrainage.
 	 * @var array
 	 */
-	public static $LISTE_DONS = array('15' => '15 €',
-	                         '20' => '20 €',
-	                         '0' => 'Autres');
+	public static $LISTE_DONS = array('5' => '5€',
+                                    '10' => '10€',
+                                    '15' => '15 €',
+                                    '20' => '20 €',
+                                    '0' => 'Autres');
 
 	/**
 	 * Oui/Non pour les formulaires.
@@ -134,6 +136,7 @@ class FP_Util_Constantes {
 	const CHAT_FICHES_A_PLACER = 8;
 	const CHAT_A_STERILISER = 9;
 	const CHAT_FICHES_A_ADOPTION_NON_RES = 10;
+	const CHAT_CHGT_PROPRIETAIRE = 11;
 	
 	/** Templates pour les mails. */
 	const MAIL_TEMPLATE_VACCINS_FA = 'rappelsVaccins.phtml';
@@ -143,6 +146,7 @@ class FP_Util_Constantes {
 	const MAIL_PARRAINER_SUJET = 'Formulaire de parrainage';
 	const MAIL_ADOPTER_SUJET = 'Formulaire d\'adoption';
 	const MAIL_BE_FA_SUJET = 'Formulaire Famille d\'accueil';
+	const MAIL_VETO = 'Formulaire vétérinaire';
 	const MAIL_RAPPEL_VACCINS_SUJET = 'Rappels de vaccins de ';
 	const MAIL_RAPPEL_STERILISATION_SUJET = 'Rappel pour la stérilisation de ';
 	
